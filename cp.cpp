d7866fb8cb40681a9c07dddbbf951af0e98bb533
Shourav Chowdhury <62851344+Shourav-pixel@users.noreply.github.com>
2023-12-30 01:57:38 +0600

    Add files via upload

---
+++ cp.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int solve(vector<ll>&v,ll n)
+{
+    map<ll,ll>mp;
+    ll sum =0,maxi=0;
+    for(ll i=0;i<n;i++)
+    {
+        sum+=v[i];
+       if(sum == 0)
+            maxi = i+1;
+       else{
+        if(mp.find(sum)!=mp.end())
+        {
+            maxi = max(maxi,i - mp[sum]);
+        }
+        else
+            mp[sum] = i;
+       }
+    }
+    return maxi;
+}
+
+int  main()
+{
+   ll t;
+   cin>>t;
+   while(t--)
+   {
+        ll n;
+        cin>>n;
+        vector<ll>v(n);
+        for(int i=0;i<n;i++)
+            cin>>v[i];
+
+        cout<<solve(v,n)<<endl;
+
+
+
+
+   }
+
+
+
+}
